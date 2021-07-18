461ebf5dc9cb7f5df31f18b48b582bffb500d0a8
Alve Larsson <larsalvelarsson@gmail.com>
2021-07-18 16:33:45 +0200

    Change allocator for smaller wasm

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 use dominator::{Dom, class, html, clone, events};
 use wasm_bindgen::prelude::*;
 
+extern crate wee_alloc;
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
 struct App {}
 
 impl App {
@@ -32,3 +36,4 @@ pub fn main() {
     let app = App::new();
     dominator::append_dom(&dominator::body(), app.render());
 }
+
